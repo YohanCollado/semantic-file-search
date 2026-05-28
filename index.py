486f49ee8b4cb4b83d98783742d74042005edfed
YohanCollado <yohancollado@yahoo.com>
2026-05-27 22:33:48 -0400

    Text was embedded and set into vectors

--- index.py
+++ index.py
@@ -1,14 +1,18 @@
 import os # python access to files/folders
 import sys # python can read terminal arguments
 
+from sentence_transformers import SentenceTransformer # library to convert text into vectors
+
 SUPPORT_EXTENSION = [".py", ".js", ".ts", ".tsx"] #read only these files, coding files, not other things
+
+model = SentenceTransformer("all-MiniLM-L6-v2") # downloads pretrained embedding model
 
 def scan_files(directory):
     
     chunks = [] # store all code we find
 
     for root, dirs, files in os.walk(directory): # os.walks(), walks through the folder recursively
-        dirs[:] = [d for d in dirs if d not in ["venv", ".git", "__pycache"]] # folders not to go in to
+        dirs[:] = [d for d in dirs if d not in ["venv", ".git", "__pycache__"]] # folders not to go in to
 
         for file in files: # loop through each file in current folder
             if any(file.endswith(ext) for ext in SUPPORT_EXTENSION): # make sure each file ends with the correct extension
@@ -38,7 +42,19 @@ if __name__ == "__main__": # run if executed directly
     folder_path = sys.argv[1] #grabs folder path from terminal
     chunks = scan_files(folder_path) # calls scanner function to scan through files
 
-    print(f"\nFound{len(chunks)} supported files:\n") # prints number of files found
+    texts = [chunk["text"] for chunk in chunks] # extracts file content
+
+    embeddings = model.encode(texts) # converts chunks into vectors
+
+    print(f"\nGenerated {len(embeddings)} embeddings\n")
+
+    if len (chunks) == 0:
+        print("No supported file was found.")
+        sys.exit(0)
+
+    print(embeddings[0]) # prints first embedding vectors
+
+    print(f"\nFound {len(chunks)} supported files:\n") # prints number of files found
 
     for chunk in chunks:
         print(chunk["file"]) # prints file path 
