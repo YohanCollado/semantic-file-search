d3175ed9c7248d957d8873460dcf148f71bb0848
YohanCollado <yohancollado@yahoo.com>
2026-05-27 20:34:49 -0400

    Function for file scanning was implemented

---
+++ index.py
@@ -0,0 +1,44 @@
+import os # python access to files/folders
+import sys # python can read terminal arguments
+
+SUPPORT_EXTENSION = [".py", ".js", ".ts", ".tsx"] #read only these files, coding files, not other things
+
+def scan_files(directory):
+    
+    chunks = [] # store all code we find
+
+    for root, dirs, files in os.walk(directory): # os.walks(), walks through the folder recursively
+        dirs[:] = [d for d in dirs if d not in ["venv", ".git", "__pycache"]] # folders not to go in to
+
+        for file in files: # loop through each file in current folder
+            if any(file.endswith(ext) for ext in SUPPORT_EXTENSION): # make sure each file ends with the correct extension
+                filepath = os.path.join(root, file) #build a path for the files
+
+                try:
+                    with open(filepath, "r", encoding="utf-8") as f: # open a file and read it = "r" = read mode
+                        content = f.read() # opens files and reads the content inside it
+
+                        chunks.append({ # stores the file in memory
+                            "file": filepath,
+                            "text": content
+                        })
+
+                except Exception as e: # if read is failed, program continues 
+                    print(f"Could not read {filepath}: {e}")
+    return chunks
+
+
+if __name__ == "__main": # run if executed directly 
+    if len(sys.args) < 2:
+        print("Usage: python index.py <folder_payh>")
+        sys.exit(1)
+        # if user forgets the folder path this it catches that
+        # tells user how to run it and exit
+
+    folder_path = sys.argv[1] #grabs folder path from terminal
+    chunks = scan_files(folder_path) # calls scanner function to scan through files
+
+    print(f"\nFound{len(chunks)} supported files:\n") # prints number of files found
+
+    for chunk in chunks:
+        print(chunk["files"]) # prints file path 
